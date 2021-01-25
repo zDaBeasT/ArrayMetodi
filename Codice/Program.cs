e15cef763cc35bdb0b9d71f8b79c060a5f6df468
Daniele Hanelli <hanellidaniele@gmail.com>
2021-01-25 19:50:23 +0100

    Add project files.

---
+++ Codice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Metodi;
+
+namespace Codice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Calcoli.Somma(array);
+        }
+    }
+}
---
+++ Metodi/Calcoli.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Metodi
+{
+    public class Calcoli
+    {
+        public static void Somma(int[] array)
+        {
+            int somma = 0;
+            for (int i=0; i < array.Length; i++)
+            {
+                somma = somma + array[i];
+
+            }
+            Console.Write($"La somma è: {somma} ");
+            Console.ReadLine();
+        }
+    }
+}
